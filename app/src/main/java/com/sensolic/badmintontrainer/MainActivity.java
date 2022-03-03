c5e40ace7cb724467dea6c8ccedf9317ad25af4a
Leolingio <67865068+Leolingio@users.noreply.github.com>
2022-03-03 16:32:32 +0100

    BADTRAIN-8 Remove exit button
    Successfully removed exit button from layout and corresponding method closeApp().

--- app/src/main/java/com/sensolic/badmintontrainer/MainActivity.java
+++ app/src/main/java/com/sensolic/badmintontrainer/MainActivity.java
@@ -1,16 +1,10 @@
 package com.sensolic.badmintontrainer;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.ContextMenu;
-import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
 
-import java.security.acl.Group;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,10 +17,6 @@ public class MainActivity extends AppCompatActivity {
     public void startTraining(View view){
         Intent intent = new Intent(this, StartActivity.class);
         startActivity(intent);
-    }
-
-    public void closeApp(View view) {
-        finish();
     }
 
     public void openSettings(View view) {
